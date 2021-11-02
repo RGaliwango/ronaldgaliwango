556aa38f8091bb40a7d2ddd84670172f68762dbd
Ronald Galiwango <galiwango.gronald.ronald@gmail.com>
2021-11-02 15:45:44 +0300

    Updated home page

--- Setting up.R
+++ Setting up.R
@@ -1,2 +1,26 @@
 install.packages('distill')
 library(distill)
+
+My interest generally lies in using computational approaches to answer epidemiology research questions. 
+
+These computational approaches include:
+  *Mathematical and statistical modelling
+*Social and contact network building an analysis
+*Genomic data analysis including Whole Genome Sequence Analysis
+*Data science and machine learning (AI)
+
+The main data of interest includes:
+  *Social and contact networks 
+*Mobility patterns 
+*Air quality data
+*Data on meteorological conditions (weather data)
+*Pathogen genomic data (such as Whole Genome Sequence data)
+*Human Genetic data
+*Other epidemiological data of individuals 
+
+My focus is on using the above computational approaches and data to aid in: 
+  *Understanding the transmission of infectious diseases (including patters and drivers) and the interplay between infectious and non-communicable diseases.
+*Identifying risk factors for (predictors of) disease and disease outcomes.
+*Evaluating the impact of health related interventions
+*Formulation and analysis of mathematical and statistical models
+*Determining health, social and economic impacts of health related social shocks such as pandemics
